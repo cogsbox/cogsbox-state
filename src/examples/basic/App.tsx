313f116052e4a907288b53098b8ed80149d4359c
djcmurphy <djcmurphy@hotmail.com>
2025-02-23 14:15:30 +0000

    2025-02-23

--- src/examples/basic/App.tsx
+++ src/examples/basic/App.tsx
@@ -71,7 +71,7 @@ export default function App() {
                     </SyntaxHighlighter>
                 </div>
             </div>
-            <LivePreview />
+            {/* <LivePreview /> */}
         </div>
     );
 }
