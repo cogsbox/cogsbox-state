1cd6adf712734460f76d0a922e7dc4b65d7be038
djcmurphy <djcmurphy@hotmail.com>
2026-04-04 09:43:59 +0100

    2026-04-04

--- dist/pluginStore.d.ts
+++ dist/pluginStore.d.ts
@@ -1,6 +1,3 @@
-import { StateObject, UpdateTypeDetail } from './CogsState';
-import { CogsPlugin } from './plugins';
-
 export type ClientActivityEvent = {
     stateKey: string;
     path: string[];
@@ -54,28 +51,5 @@ export type ClientActivityEvent = {
         cursorPosition: number;
     };
 });
-type PluginRegistryStore = {
-    stateHandlers: Map<string, StateObject<any>>;
-    registerStateHandler: (stateKey: string, handler: StateObject<any>) => void;
-    registeredPlugins: readonly CogsPlugin<any, any, any, any, any>[];
-    setRegisteredPlugins: (plugins: readonly CogsPlugin<any, any, any, any, any>[]) => void;
-    pluginOptions: Map<string, Map<string, any>>;
-    setPluginOptionsForState: (stateKey: string, pluginOptions: Record<string, any>) => void;
-    getPluginConfigsForState: (stateKey: string) => Array<{
-        plugin: CogsPlugin<any, any, any, any, any>;
-        options: any;
-    }>;
-    updateSubscribers: Set<(update: UpdateTypeDetail) => void>;
-    subscribeToUpdates: (callback: (update: UpdateTypeDetail) => void) => () => void;
-    notifyUpdate: (update: UpdateTypeDetail) => void;
-    formUpdateSubscribers: Set<(event: ClientActivityEvent) => void>;
-    subscribeToFormUpdates: (callback: (event: ClientActivityEvent) => void) => () => void;
-    notifyFormUpdate: (event: ClientActivityEvent) => void;
-    hookResults: Map<string, Map<string, any>>;
-    setHookResult: (stateKey: string, pluginName: string, data: any) => void;
-    getHookResult: (stateKey: string, pluginName: string) => any | undefined;
-    removeHookResult: (stateKey: string, pluginName: string) => void;
-};
-export declare const pluginStore: import('zustand').UseBoundStore<import('zustand').StoreApi<PluginRegistryStore>>;
-export {};
+export declare const pluginStore: any;
 //# sourceMappingURL=pluginStore.d.ts.map
